d1738693ca9aee87153c21ff117a79ef941fd5fc
chad rehm <chad.rehm@kineticdata.com>
2017-05-21 10:56:29 -0500

    initial commit

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Secret Recipe
+      <ul>
+        <li>Cup of surger</li>
+        <li>Cup of salt</li>
+        <li>Cup of pepper</li>
+      </ul>
+    </div>
+  );
+}
